77d6b533259bd95db4a8d730b5fc2c1b9bdf7c93
xx7841 <xx7841@gmail.com>
2018-11-25 20:58:13 +0900

    Code Optimization

--- HeightMapTool.Designer.cs
+++ HeightMapTool.Designer.cs
@@ -1,11 +1,8 @@
-﻿namespace HeightMapTool
-{
-    partial class HeightMapTool
-    {
+﻿namespace HeightMapTool {
+    partial class HeightMapTool {
         private System.ComponentModel.IContainer components = null;
 
-        protected override void Dispose(bool disposing)
-        {
+        protected override void Dispose(bool disposing) {
             if (disposing && (components != null)) {
                 components.Dispose();
             }
@@ -13,17 +10,14 @@
         }
 
         #region Windows Form Designer Code
-        private void InitializeComponent()
-        {
+        private void InitializeComponent() {
             this.menuStrip = new System.Windows.Forms.MenuStrip();
             this.menu_LoadHeightMap = new System.Windows.Forms.ToolStripMenuItem();
             this.menu_SaveHeightMap = new System.Windows.Forms.ToolStripMenuItem();
             this.menu_Reset = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip.SuspendLayout();
             this.SuspendLayout();
-            //
-            // Form
-            //
+
             this.Text = "HeightMap Tool";
             this.components = new System.ComponentModel.Container();
             this.ClientSize = new System.Drawing.Size(1600, 900);
@@ -33,23 +27,17 @@
             this.Controls.Add(this.menuStrip);
             this.menuStrip.ResumeLayout(false);
             this.ResumeLayout(false);
-            //
-            // MenuStrip
-            //
+
+
+
             this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { this.menu_LoadHeightMap, this.menu_SaveHeightMap, this.menu_Reset });
-            //
-            // MenuStrip → Load HeightMap
-            //
+
             this.menu_LoadHeightMap.Text = "Load HeightMap";
             this.menu_LoadHeightMap.Click += new System.EventHandler(this.MenuItem_LoadHeightMap_Click);
-            //
-            // MenuStrip → Save HeightMap
-            //
+
             this.menu_SaveHeightMap.Text = "Save HeightMap";
             this.menu_SaveHeightMap.Click += new System.EventHandler(this.MenuItem_SaveHeightMap_Click);
-            //
-            // MenuStrip → Reset
-            //
+
             this.menu_Reset.Text = "Reset";
             this.menu_Reset.Click += new System.EventHandler(this.MenuItem_Reset_Click);
         }
